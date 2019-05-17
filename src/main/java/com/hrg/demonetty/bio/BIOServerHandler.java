d0a71ccefa7035bb5fb0e9880c6431cb2e255f35
haorengao <872248907@qq.com>
2019-05-17 15:13:06 +0800

    init project !

---
+++ src/main/java/com/hrg/demonetty/bio/BIOServerHandler.java
@@ -0,0 +1,50 @@
+package com.hrg.demonetty.bio;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class BIOServerHandler implements Runnable{  
+    
+  private Socket socket;  
+  public BIOServerHandler(Socket socket) {  
+      this.socket = socket;  
+  }  
+  @Override  
+  public void run() {  
+      BufferedReader reader = null;  
+      PrintWriter writer = null;  
+      try {  
+          reader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));  
+          writer = new PrintWriter(this.socket.getOutputStream(), true);  
+          String body = null;  
+          while (true) {  
+              body = reader.readLine(); // 若客户端用的是 writer.print() 传值，那readerLine() 是不能获取值，细节  
+              if (null == body) {  
+                  break;  
+              }  
+              System.out.println("server服务端接收参数 : " + body);  
+              writer.println(body);
+//              writer.println(body + " = " + CalculatorUtil.cal(body).toString());
+          }  
+      } catch (IOException e) {  
+          e.printStackTrace();  
+      } finally {  
+          if (null != writer) {  
+              writer.close();  
+          }  
+          try {  
+              if (null != reader) {  
+                  reader.close();  
+              }  
+              if (null != this.socket) {  
+                  this.socket.close();  
+                  this.socket = null;  
+              }  
+          } catch (IOException e) {  
+              e.printStackTrace();  
+          }  
+      }  
+  }  
+}
